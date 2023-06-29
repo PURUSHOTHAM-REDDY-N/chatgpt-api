87c78f1d36cbbe795a78a5beeb72853755fe976c
PURUSHOTHAM-REDDY-N <84611950+PURUSHOTHAM-REDDY-N@users.noreply.github.com>
2023-06-29 14:38:37 +0530

    cors fixed

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const cors = require('cors');
+
 require("dotenv").config();
 const { Configuration, OpenAIApi } = require("openai");
 
 const app = express();
-
+app.use(cors());
 app.use(express.json());
 
 const configuration = new Configuration({
