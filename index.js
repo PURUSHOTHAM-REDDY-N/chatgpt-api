3e63d54f112571b3ba1457313d9ff4aa51020760
PURUSHOTHAM-REDDY-N <84611950+PURUSHOTHAM-REDDY-N@users.noreply.github.com>
2023-07-04 12:05:54 +0530

    updated it to memorize the previous messages

--- index.js
+++ index.js
@@ -13,34 +13,24 @@ const configuration = new Configuration({
 });
 const openai = new OpenAIApi(configuration);
 
-app.post("/gpt", async (req, res) => {
-  try {
-    const { prompt } = req.body;
-    const response = await openai.createCompletion({
-      model: "text-davinci-003",
-      prompt: `
-              ${prompt}
-            `,
-      max_tokens: 3000,
-      // temperature: 0,
-      // top_p: 1.0,
-      // frequency_penalty: 0.0,
-      // presence_penalty: 0.0,
-      // stop: ["\n"],
-    });
+app.post("/gpt", async (request, response) => {
+  const { chats } = request.body;
+  console.log(chats)
 
-    return res.status(200).json({
-      success: true,
-      data: response.data.choices[0].text,
-    });
-  } catch (error) {
-    return res.status(400).json({
-      success: false,
-      error: error.response
-        ? error.response.data
-        : "There was an issue on the server",
-    });
-  }
+  const result = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    messages: [
+      {
+        role: "system",
+        content: "You are a Conditional Fund Raising Assistant. You can help campaign creators to define the creative conditions for their campaigns.",
+      },
+      ...chats,
+    ],
+  });
+
+  response.json({
+    output: result.data.choices[0].message,
+  });
 });
 
 
