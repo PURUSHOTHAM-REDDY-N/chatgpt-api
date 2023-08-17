eaa75f5e1458354f07259a018edfbde0fc4905b0
Varun Kodi <varun@7dotss.com>
2023-08-17 18:06:54 +0530

    adjusted response from chatGPT

--- index.js
+++ index.js
@@ -9,88 +9,116 @@ app.use(cors());
 app.use(express.json());
 
 const configuration = new Configuration({
-  apiKey: process.env.OPEN_AI_KEY,
+    apiKey: process.env.OPEN_AI_KEY,
 });
 const openai = new OpenAIApi(configuration);
 
 app.post("/gpt", async (req, res) => {
-  try {
-    const {prompt} = req.body;
-    const response = await openai.createCompletion({
-      model: "text-davinci-003",
-      prompt: `
+    try {
+        const {prompt} = req.body;
+        const response = await openai.createCompletion({
+            model: "text-davinci-003",
+            prompt: `
               ${prompt}
             `,
-      max_tokens: 3000,
-      // temperature: 0,
-      // top_p: 1.0,
-      // frequency_penalty: 0.0,
-      // presence_penalty: 0.0,
-      // stop: ["\n"],
-    });
+            max_tokens: 3000,
+            // temperature: 0,
+            // top_p: 1.0,
+            // frequency_penalty: 0.0,
+            // presence_penalty: 0.0,
+            // stop: ["\n"],
+        });
 
-    return res.status(200).json({
-      success: true,
-      data: response.data.choices[0].text,
-    });
-  } catch (error) {
-    return res.status(400).json({
-      success: false,
-      error: error.response
-          ? error.response.data
-          : "There was an issue on the server",
-    });
-  }
+        return res.status(200).json({
+            success: true,
+            data: response.data.choices[0].text,
+        });
+    } catch (error) {
+        return res.status(400).json({
+            success: false,
+            error: error.response
+                ? error.response.data
+                : "There was an issue on the server",
+        });
+    }
 });
 
-app.post('/createTasks', async (req, res)=> {
-  try {
-    // throw QueryEmptyError
-    const reqBody = req.body
+app.post('/createTasks', async (req, res) => {
+    let lastChat;
+    try {
+        // throw QueryEmptyError
+        const reqBody = req.body
+        let chats = reqBody.chat || [];
+        console.log('ReqBody is \n', reqBody);
+        // console.log('Before \n', chats);
+        let prompt = reqBody.prompt;
+        // if (reqBody.person) {
+        //     chats.push({
+        //         role: 'user',
+        //         content: prompt
+        //     })
+        // }
+        let response = null;
 
-    // console.log(body);
+        // console.log('After \n', chats);
+        chats ?
+        response = await openai.createCompletion({
+            model: "text-davinci-003",
+            prompt: prompt,
+            max_tokens: 3000,
+            // temperature: 0,
+            // top_p: 1.0,
+            // frequency_penalty: 0.0,
+            // presence_penalty: 0.0,
+            // stop: ["\n"],
+        })
+        :
+        response = await openai.createChatCompletion({
+            model: "text-davinci-003",
+            messages: chats,
+            prompt: prompt,
+            max_tokens: 3000,
+            // temperature: 0,
+            // top_p: 1.0,
+            // frequency_penalty: 0.0,
+            // presence_penalty: 0.0,
+            // stop: ["\n"],
+        });
+        console.log('response data = ', response.data);
 
-    const response = await openai.createCompletion({
-      model: "text-davinci-003",
-      prompt: `
-              generate appropriate tasks for the person '${reqBody.person}' based on the number of keywords in '${reqBody.tasks}'.
-              The task must be in the form of '${reqBody.person}' must do (some task)
-              give me the reply in json format with Name field and Description field enclosed in Campaigns array
-              `,
-      max_tokens: 3000,
-      // temperature: 0,
-      // top_p: 1.0,
-      // frequency_penalty: 0.0,
-      // presence_penalty: 0.0,
-      // stop: ["\n"],
-    });
+        chats.push({
+            role: 'system',
+            content: response.data.choices[0].text,
+        })
 
-    // const formattedResponse = await openai.createCompletion({
-    //   model: "text-davinci-003",
-    //   prompt: `
-    //       Format the below ${response.data.choices[0].text} to proper JSON Object
-    //           `,
-    //   max_tokens: 3000,
-    //   // temperature: 0,
-    //   // top_p: 1.0,
-    //   // frequency_penalty: 0.0,
-    //   // presence_penalty: 0.0,
-    //   // stop: ["\n"],
-    // });
-    // console.log(JSON.parse(formattedResponse.data.choices[0].text));
+        try {
+            CampaignsResponse = JSON.parse(response.data.choices[0].text.replace(/'/ig, '"'));
 
-    return res.status(200).json(
-        {
-          data: response.data.choices[0].text,
+            console.log('Result Campaigns = ', CampaignsResponse)
+
+        } catch (e) {
+            return res.status(500).json({
+                error: 'Error in parsing'
+            })
         }
-    );
-  } catch (e) {
-    console.log(e);
-    return res.status(400).json({
-      success: false,
-      error: e.type
-    })
-  }
+
+
+        return res.status(200).json(
+            {
+                Campaigns: CampaignsResponse.Campaigns || CampaignsResponse,
+                chat: chats,
+                person: reqBody.person,
+                tasks: reqBody.tasks,
+            }
+        );
+    } catch
+        (e) {
+        console.log(e);
+        return res.status(400).json({
+            success: false,
+            error: e.type
+        })
+    }
 
 })
 
